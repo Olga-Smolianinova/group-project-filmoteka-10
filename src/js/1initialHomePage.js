bea3973060e3c253bd3ea7f9c3cca40c232c4f75
Olga-Smolianinova <olga.smol2015@gmail.com>
2021-01-30 18:53:59 +0200

    test

--- src/js/1initialHomePage.js
+++ src/js/1initialHomePage.js
@@ -19,30 +19,44 @@ const renderFilms = 0;
 const genres = 0;
 const pageNumber = 1;
 
-function createCardFunc(imgPath, filmTitle, movieId) {
-  //
+// для отрисовки фильмов через template  в HTML
+function createCardFunc(data) {
+  const markup = templateListOfFilms(data);
+
+  // встраиваем полученные данные в HTML документ
+  refs.galleryRef.insertAdjacentHTML('beforeend', markup);
 }
 
+// fetch запрос на список самых популярных фильмов на сегодня для создания коллекции на главной странице:
 function fetchPopularMoviesList() {
   const url =
-    'https://api.themoviedb.org/3/trending/movie/week?api_key=a524e22e3630cf24a2e0a24a461145a2';
+    'https://api.themoviedb.org/3/trending/movie/day?api_key=a524e22e3630cf24a2e0a24a461145a2';
+
+  return fetch(url)
+    .then(response => {
+      // console.log(response);
+      return response.json();
+    })
+    .then(({ results }) => {
+      console.log(results);
+      createCardFunc(results);
+    });
+}
+fetchPopularMoviesList();
+
+// fetch запрос на список самых популярных фильмов на сегодня для создания коллекции на главной странице:
+function fetchGenres() {
+  const url =
+    'https://api.themoviedb.org/3/genre/movie/list?api_key=a524e22e3630cf24a2e0a24a461145a2';
 
   return fetch(url)
     .then(response => {
       console.log(response);
       return response.json();
     })
-    .then(({ results }) => {
-      console.log(results);
-      createListOfFilms(results);
+    .then(({ genres }) => {
+      console.log(genres);
+      // createCardFunc(genres);
     });
 }
-
-fetchPopularMoviesList();
-
-function createListOfFilms(data) {
-  const markup = templateListOfFilms(data);
-
-  // встраиваем полученные данные в HTML документ
-  refs.galleryRef.insertAdjacentHTML('beforeend', markup);
-}
+fetchGenres();
