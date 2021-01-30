062ef0031af4efad9fe25bc61dec4047a99d82e5
Oleh Sydorenko <oleh.sydorenko1994@gmail.com>
2021-01-30 19:26:14 +0200

    Добавляет розметку для футера и модал

---
+++ src/js/footer.js
@@ -0,0 +1,38 @@
+import markupFooter from '../html/footer.html';
+
+console.log(markupFooter);
+
+const refs = {
+    footer: document.querySelector('.footer')
+}
+
+refs.footer.insertAdjacentHTML('afterend', markupFooter)
+
+
+// =====================================>
+const openModalRef = document.querySelector('.link-modal');
+const lightboxOverlayRef = document.querySelector('.lightbox__overlay');
+const lightboxImageRef = document.querySelector('.lightbox__image');
+const lightboxButtonRef = document.querySelector('.lightbox__button');
+const lightboxRef = document.querySelector('.lightbox');
+
+openModalRef.addEventListener('click', onOpenModal)
+lightboxOverlayRef.addEventListener('click', onCloseModal)
+
+function onOpenModal(event) {
+    event.preventDefault();
+    lightboxRef.classList.add('is-open')
+    window.addEventListener('keydown', onCloceModalESC)
+}
+
+function onCloseModal() {
+    lightboxRef.classList.remove('is-open');
+    window.removeEventListener('keydown', onCloceModalESC);
+}
+
+function onCloceModalESC(event) {
+    if (event.code === 'Escape') {
+        onCloseModal();
+    }
+}
+
