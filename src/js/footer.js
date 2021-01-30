8685c70d698e8cb5bf201fc44ad9ceacea562ff5
Olga-Smolianinova <olga.smol2015@gmail.com>
2021-01-31 00:16:31 +0200

    test

--- src/js/footer.js
+++ src/js/footer.js
@@ -1,14 +1,11 @@
-
 import markupFooter from '../html/footer.html';
-
-console.log(markupFooter);
+// console.log(markupFooter);
 
 const refs = {
-    footer: document.querySelector('.footer')
-}
-
-refs.footer.insertAdjacentHTML('afterend', markupFooter)
-
+  footer: document.querySelector('footer'),
+};
+console.log(refs);
+refs.footer.insertAdjacentHTML('afterend', markupFooter);
 
 // =====================================>
 const openModalRef = document.querySelector('.link-modal');
@@ -17,24 +14,22 @@ const lightboxImageRef = document.querySelector('.lightbox__image');
 const lightboxButtonRef = document.querySelector('.lightbox__button');
 const lightboxRef = document.querySelector('.lightbox');
 
-openModalRef.addEventListener('click', onOpenModal)
-lightboxOverlayRef.addEventListener('click', onCloseModal)
+openModalRef.addEventListener('click', onOpenModal);
+lightboxOverlayRef.addEventListener('click', onCloseModal);
 
 function onOpenModal(event) {
-    event.preventDefault();
-    lightboxRef.classList.add('is-open')
-    window.addEventListener('keydown', onCloceModalESC)
+  event.preventDefault();
+  lightboxRef.classList.add('is-open');
+  window.addEventListener('keydown', onCloceModalESC);
 }
 
 function onCloseModal() {
-    lightboxRef.classList.remove('is-open');
-    window.removeEventListener('keydown', onCloceModalESC);
+  lightboxRef.classList.remove('is-open');
+  window.removeEventListener('keydown', onCloceModalESC);
 }
 
 function onCloceModalESC(event) {
-    if (event.code === 'Escape') {
-        onCloseModal();
-    }
+  if (event.code === 'Escape') {
+    onCloseModal();
+  }
 }
-
-
