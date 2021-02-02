ffe885d115e3ecb946c39ebf1695c98eaad81714
Olga-Smolianinova <olga.smol2015@gmail.com>
2021-02-03 00:12:17 +0200

    test

--- src/js/refs.js
+++ src/js/refs.js
@@ -1,14 +1,15 @@
 const refs = {
+  galleryRef: document.querySelector('.gallery'),
 
-  gallery: document.querySelector('.gallery'),
+  main: document.querySelector('main'), // ссылка откуда надо брать названия фильмов для локал сторедж
 
   footer: document.querySelector('footer'),
-  main: document.querySelector('main'), // ссылка откуда надо брать названия фильмов для локал сторедж
-  galleryRef: document.querySelector('.gallery'),
+
   myLibraryBtn: document.querySelector('.library'),
   backgroundHome: document.querySelector('.header-container'),
   bntlibrary: document.querySelector('.form-button-library'),
   inputForm: document.querySelector('.search-box-home'),
-  
+
+  // Модальне вікно
 };
 export default refs;
