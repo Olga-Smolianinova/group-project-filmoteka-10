0861b24b066cbda5ec544597a544ceb7767ad2f8
Yaroslaw Dmukhovsckyi <ermacus096@gmail.com>
2021-02-03 12:31:27 +0200

    Добавляет переход на главную страницу

---
+++ src/js/3navigation.js
@@ -0,0 +1,22 @@
+import refs from '../js/refs'
+import {createCardFunc, fetchPopularMoviesList} from './1initialHomePage.js'
+
+refs.homeBtn.addEventListener("click", activeHomePage)
+refs.logo.addEventListener("click", activeHomePage)
+
+function activeHomePage(event) {
+    event.preventDefault()
+    refs.galleryRef.innerHTML = ''
+    createCardFunc()
+    fetchPopularMoviesList()
+    
+    addClassHome()
+}
+
+function addClassHome() {
+    refs.backgroundHome.classList.remove('header-background-library');
+    refs.backgroundHome.classList.add('header-background-home');
+
+    refs.inpuForm.classList.remove('is-hidden');
+    refs.bntlibrary.classList.add('is-hidden');
+}
--- src/js/refs.js
+++ src/js/refs.js
@@ -6,9 +6,11 @@ const refs = {
   footer: document.querySelector('footer'),
 
   myLibraryBtn: document.querySelector('.library'),
+  homeBtn: document.querySelector('.home'),
   backgroundHome: document.querySelector('.header-container'),
   bntlibrary: document.querySelector('.form-button-library'),
   inputForm: document.querySelector('.search-box-home'),
+  logo: document.querySelector('.logo'),
 
   // Модальне вікно
 };
